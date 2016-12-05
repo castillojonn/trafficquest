e15a4c7aa06e56adba564f8c72d51fc0877b338b
Parth Choksi <pkchoksi39@gmail.com>
2016-12-05 13:35:25 -0500

    DONOT COMMIT CODE UNLESS YOU KNOW WHAT IT DOES. Fixed the app.

--- app/src/main/java/pkc/trafficquest/sccapstone/trafficquest/MainActivity.java
+++ app/src/main/java/pkc/trafficquest/sccapstone/trafficquest/MainActivity.java
@@ -1,11 +1,9 @@
 package pkc.trafficquest.sccapstone.trafficquest;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,18 +14,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -41,16 +27,9 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private static final int ERROR_DIALOG_REQUEST = 9001;
-    GoogleMap mMap;
-    private static final double
-            COLUMBIA_LAT = 33.99882,
-            COLUMBIA_LNG = -81.04537,
-            ATLANTA_LAT = 33.74831,
-            ATLANTA_LNG = -84.39111,
-            ORLANDO_LAT = 28.53823,
-            ORLANDO_LNG = -81.37739;
+public class MainActivity extends AppCompatActivity{
+
+
 
 
 
@@ -65,14 +44,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReferenceFromUrl(FIREBASE_URL); // reference to the Firebase path
-
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //Hello welcome to the code
@@ -80,23 +59,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-
-        if (servicesOK()) {
-            setContentView(R.layout.activity_map);
-            if (initMap()) {
-                Toast.makeText(this, "Ready to Map!", Toast.LENGTH_SHORT).show();
-                gotoLocation(COLUMBIA_LAT, COLUMBIA_LNG, 10);
-            }
-            else {
-                Toast.makeText(this, "Map no connected!", Toast.LENGTH_SHORT).show();
-        }
 
 
-        }
-        else {
-            setContentView(R.layout.activity_main);
-        }
     }
 
     @Override
@@ -127,6 +91,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             mAuth.signOut();
             startActivity(new Intent(getApplicationContext(), DispatchActivity.class));
             return true;
+        }
+        else if(id == R.id.action_Maps){
+            startActivity(new Intent(getApplicationContext(),MapsActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
@@ -266,87 +233,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
-
-   /* private class myTask extends AsyncTask<RequestPackage,String,String>{
-        @Override
-        protected void onPreExecute() {
-            //super.onPreExecute();
-
-        }
-        @Override
-        protected String doInBackground(RequestPackage... strings) {
-            return "task Complete";
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            //super.onPostExecute(s);
-            toastMaker(s);
-        }
-    }
-    */
-    public Boolean servicesOK () {
-        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (isAvailable == ConnectionResult.SUCCESS) {
-            return true;
-        }
-        else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
-            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
-            dialog.show();
-        }
-        else {
-            Toast.makeText(this, "Can't connect to mapping service", Toast.LENGTH_SHORT).show();
-        }
-
-        return false;
-    }
-    private boolean initMap() {
-        if (mMap == null) {
-            MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-        }
-        return (mMap != null);
-    }
-
-    private void gotoLocation(double lat, double lng, float zoom) {
-        LatLng latLng = new LatLng(lat, lng);
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
-        mMap.moveCamera(update);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-    }
 }
---
+++ app/src/main/java/pkc/trafficquest/sccapstone/trafficquest/MapsActivity.java
@@ -0,0 +1,154 @@
+package pkc.trafficquest.sccapstone.trafficquest;
+
+import android.app.Dialog;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    GoogleMap mMap;
+    private static final double
+            COLUMBIA_LAT = 33.99882,
+            COLUMBIA_LNG = -81.04537,
+            ATLANTA_LAT = 33.74831,
+            ATLANTA_LNG = -84.39111,
+            ORLANDO_LAT = 28.53823,
+            ORLANDO_LNG = -81.37739;
+    private GoogleApiClient client;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        if (servicesOK()) {
+            setContentView(R.layout.activity_map);
+            if (initMap()) {
+                Toast.makeText(this, "Ready to Map!", Toast.LENGTH_SHORT).show();
+                gotoLocation(COLUMBIA_LAT, COLUMBIA_LNG, 10);
+            }
+            else {
+                Toast.makeText(this, "Map no connected!", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    /* private class myTask extends AsyncTask<RequestPackage,String,String>{
+         @Override
+         protected void onPreExecute() {
+             //super.onPreExecute();
+
+         }
+         @Override
+         protected String doInBackground(RequestPackage... strings) {
+             return "task Complete";
+         }
+
+         @Override
+         protected void onPostExecute(String s) {
+             //super.onPostExecute(s);
+             toastMaker(s);
+         }
+     }
+     */
+    public Boolean servicesOK () {
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        }
+        else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, "Can't connect to mapping service", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
+    private boolean initMap() {
+        if (mMap == null) {
+            MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+        return (mMap != null);
+    }
+
+    private void gotoLocation(double lat, double lng, float zoom) {
+        LatLng latLng = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
+        mMap.moveCamera(update);
+    }
+
+
+}
