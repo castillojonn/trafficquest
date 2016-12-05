d00872643d3ad170dff4a85dbc74e5593b2a230b
KDOOLEY102 <KDOOLEY@email.sc.edu>
2016-12-05 03:40:42 -0500

    Added a Map and Default location

--- app/src/main/java/pkc/trafficquest/sccapstone/trafficquest/MainActivity.java
+++ app/src/main/java/pkc/trafficquest/sccapstone/trafficquest/MainActivity.java
@@ -1,9 +1,11 @@
 package pkc.trafficquest.sccapstone.trafficquest;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +16,18 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -27,7 +41,19 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    GoogleMap mMap;
+    private static final double
+            COLUMBIA_LAT = 33.99882,
+            COLUMBIA_LNG = -81.04537,
+            ATLANTA_LAT = 33.74831,
+            ATLANTA_LNG = -84.39111,
+            ORLANDO_LAT = 28.53823,
+            ORLANDO_LNG = -81.37739;
+
+
+
     public static final String API_KEY = "AmJHdhFiW4EQCdWrgEoTk5-vo8zW-96v2LBmeBgnc0z_FV0Ru-gZizGCLfhtRtrJ";
     public static final String ENDPOINT = "http://dev.virtualearth.net";
     public static final String FIREBASE_URL = "https://trafficquest-9b525.firebaseio.com/";
@@ -35,18 +61,42 @@ public class MainActivity extends AppCompatActivity {
     Response<ArrayList<Accidents>> accidentses;
     private ListView view;
     private DatabaseReference mDatabase; // reference to the Firebase
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReferenceFromUrl(FIREBASE_URL); // reference to the Firebase path
-        setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //Hello welcome to the code
         view = (ListView) findViewById(R.id.aListview);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        if (servicesOK()) {
+            setContentView(R.layout.activity_map);
+            if (initMap()) {
+                Toast.makeText(this, "Ready to Map!", Toast.LENGTH_SHORT).show();
+                gotoLocation(COLUMBIA_LAT, COLUMBIA_LNG, 10);
+            }
+            else {
+                Toast.makeText(this, "Map no connected!", Toast.LENGTH_SHORT).show();
+        }
+
+
+        }
+        else {
+            setContentView(R.layout.activity_main);
+        }
     }
 
     @Override
@@ -65,38 +115,38 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            if(isOnline()){
+            if (isOnline()) {
                 requestData();
-            }
-            else{
-                Toast.makeText(getApplicationContext(),"NETWORK IS NOT AVAILABLE",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "NETWORK IS NOT AVAILABLE", Toast.LENGTH_SHORT).show();
             }
             return true;
-        }
-        else if(id == R.id.action_logout){
+        } else if (id == R.id.action_logout) {
             FirebaseUser user = mAuth.getCurrentUser();
-            Toast.makeText(getApplicationContext(),user.getEmail().toString()+ " has signed out",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), user.getEmail().toString() + " has signed out", Toast.LENGTH_SHORT).show();
             mAuth.signOut();
-            startActivity(new Intent(getApplicationContext(),DispatchActivity.class));
+            startActivity(new Intent(getApplicationContext(), DispatchActivity.class));
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-    public void toastMaker(String toast){
-        Toast.makeText(getApplicationContext(),toast,Toast.LENGTH_SHORT).show();
+
+    public void toastMaker(String toast) {
+        Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
     }
-    protected boolean isOnline(){
+
+    protected boolean isOnline() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnectedOrConnecting()){
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    protected  void requestData(){
+
+    protected void requestData() {
         Retrofit restAdapter = new Retrofit.Builder()
                 .baseUrl(ENDPOINT).addConverterFactory(GsonConverterFactory.create()).build();
         AccidentsAPI api = restAdapter.create(AccidentsAPI.class);
@@ -106,48 +156,46 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<RequestPackage> call, Response<RequestPackage> response) {
                 RequestPackage res = response.body();
                 //res = response.body();
-                try{
-                    if(response!= null){
-                        Toast.makeText(getApplicationContext(),"Message: "+response.message()+": "+response.code(),Toast.LENGTH_LONG).show();
+                try {
+                    if (response != null) {
+                        Toast.makeText(getApplicationContext(), "Message: " + response.message() + ": " + response.code(), Toast.LENGTH_LONG).show();
                         ArrayList<Accidents> accidents = new ArrayList<Accidents>();
                         ArrayList<ResourceSet> rSet = new ArrayList<ResourceSet>();
-                        for (int i = 0; i<res.getResourceSets().size();i++){
+                        for (int i = 0; i < res.getResourceSets().size(); i++) {
                             rSet.add(res.getResourceSets().get(i));
                         }
                         //ResourceSet rSetobj = new ResourceSet();
-                        for (int i = 0; i < rSet.size(); i++){
+                        for (int i = 0; i < rSet.size(); i++) {
                             ResourceSet rSetobj = new ResourceSet();
                             rSetobj = rSet.get(i);
                             accidents = rSetobj.getResources();
                             ArrayList<String> names = new ArrayList<String>();
-                            for (int j = 0; j < accidents.size(); j++){
+                            for (int j = 0; j < accidents.size(); j++) {
                                 Accidents accObj = new Accidents();
                                 accObj = accidents.get(j);
                                 names.add(accObj.getDescription());
 
                             }
-                            ArrayAdapter ap = new ArrayAdapter(getApplicationContext(),R.layout.accident_list,names);
+                            ArrayAdapter ap = new ArrayAdapter(getApplicationContext(), R.layout.accident_list, names);
                             //mDatabase.child("users").child("" + mAuth.getCurrentUser().getUid()).setValue(names); // stores the requested list into the database
-                            mDatabase.child("users").child(""+ mAuth.getCurrentUser().getUid()).child("Accidents").setValue(accidents); // stores the requested list into the database
+                            mDatabase.child("users").child("" + mAuth.getCurrentUser().getUid()).child("Accidents").setValue(accidents); // stores the requested list into the database
 
                             view.setAdapter(ap);
                             toastMaker("Task Completed");
 
                         }
 
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Message: " + response.message() + ": " + response.code(), Toast.LENGTH_LONG).show();
                     }
-                    else{
-                        Toast.makeText(getApplicationContext(),"Message: "+response.message()+": "+response.code(),Toast.LENGTH_LONG).show();
-                    }
-                }
-                catch (Exception e){
-                    Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
 
             @Override
             public void onFailure(Call<RequestPackage> call, Throwable t) {
-                Toast.makeText(getApplicationContext(),"Message: "+t.getMessage(),Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Message: " + t.getMessage(), Toast.LENGTH_LONG).show();
                 Log.d("Message", t.getMessage());
                 t.printStackTrace();
             }
@@ -207,10 +255,47 @@ public class MainActivity extends AppCompatActivity {
         });
         */
     }
-    public void saveData(ArrayList<Accidents> accList){
+
+    public void saveData(ArrayList<Accidents> accList) {
 
         mDatabase.child("users").child("Accidents").child("" + mAuth.getCurrentUser().getUid()).setValue(accList); // stores the requested list into the database
 
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 
    /* private class myTask extends AsyncTask<RequestPackage,String,String>{
@@ -231,4 +316,37 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     */
+    public Boolean servicesOK () {
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        }
+        else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, "Can't connect to mapping service", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
+    private boolean initMap() {
+        if (mMap == null) {
+            MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+        return (mMap != null);
+    }
+
+    private void gotoLocation(double lat, double lng, float zoom) {
+        LatLng latLng = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
+        mMap.moveCamera(update);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
